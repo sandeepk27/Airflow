1656465723752a64f13f7368911b54258b729c15
sandeep <nanisandeep876@gmail.com>
2024-11-05 20:35:40 +0530

    Airflow first commit

---
+++ cmd.sh
@@ -0,0 +1,4 @@
+docker-compose up airflow-init - To initilize airflow containers
+docker-compose up - to run the Airflow
+docker-compose down -v - to shut down the airflow
+docker ps - to check the status of containers
---
+++ dags/test_kubernetes_pod_operator.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+     'description': 'Test Pipeline',
+     'depend_on_past': False,
+    'start_date':  datetime(2016, 11, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'kubernetes_sample', default_args=default_args, schedule_interval= '@once')
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+
+passing = KubernetesPodOperator(namespace='airflow', image="python:3.6", cmds=["python","-c"], 
+  arguments=["print('hello world')"], name="passing-test", task_id="passing-task", get_logs=True, in_cluster=True, dag=dag )
+
+
+
+start >> passing
